c05e5cef794c6300f480bd6668042b4959a95870
vlad <vlad.bohdan.ua@gmail.com>
2020-12-06 22:59:03 +0200

    table

--- src/pages/personalAccount.vue
+++ src/pages/personalAccount.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="personalAccount">
-    <personalOffice></personalOffice>
+    <Table></Table>
+    <table2></table2>
   </div>
 </template>
 
 <script>
-import personalOffice from '../components/personalOffice'
+import Table from 'components/table'
+import Table2 from 'components/table2'
 export default {
   name: 'personalAccount',
   components: {
-    personalOffice
+    Table,
+    Table2
   }
 }
 </script>
